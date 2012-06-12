8fa3fa235d329f2aa03ad6c5d01b0c38e7e2c665
runeleaf <runeleaf@gmail.com>
2012-06-12 14:58:18 +0900

    開発用に修正

--- lib/railstar.rb
+++ lib/railstar.rb
@@ -7,7 +7,7 @@ ActionView::Base.send(:include, Railstar::Helper)
 require 'railstar/active_record_ext'
 ActiveRecord::Base.send(:include, Railstar::ActiveRecordExt)
 
-require 'railstar/column_comment' if /mysql/i =~ Rails.configuration.database_configuration[Rails.env]["adapter"]
+#require 'railstar/column_comment' if /mysql/i =~ Rails.configuration.database_configuration[Rails.env]["adapter"]
 
 module Railstar
   def self.env
